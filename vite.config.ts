829e82c24625126bdc8a624262c1d181ed440260
CristianRodrigue <rodriguez.cami09@gmail.com>
2026-02-20 21:55:17 -0500

    Actualiza configuración de Vite y agrega build

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,16 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
     plugins: [react(), tailwindcss()],
+    base: './', // Esto hace que las rutas sean relativas [citation:2][citation:8]
+    build: {
+        rollupOptions: {
+            output: {
+                entryFileNames: 'assets/[name].js',
+                chunkFileNames: 'assets/[name].js',
+                assetFileNames: 'assets/[name].[ext]'
+            }
+        }
+    },
     server: {
         host: 'localhost',
         port: 5173
